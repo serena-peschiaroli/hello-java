3acb13398807cdb80de56cb2827a8554d44d8cb2
serena.peschiaroli <serena.peschiaroli@gmail.com>
2024-03-26 14:37:53 +0100

    test with scanner obj for user input

--- src/org/learning/HelloJava.java
+++ src/org/learning/HelloJava.java
@@ -1,8 +1,10 @@
 package org.learning;
 
+import java.util.Scanner;
+
 public class HelloJava {
     public static void main(String[] args) {
-        //test with String
+        //test con String
         String greetings = "Hello, Java!";
         int myInteger = 10;
         System.out.println(greetings + " " + "My number is " + myInteger);
@@ -12,6 +14,19 @@ public class HelloJava {
         for(int i=0; i < 5; i++){
             System.out.printf("The value of i is: %d\n", i);
         }
+        //test with scanner input
+
+        //Create Scanner obj
+        Scanner input = new Scanner(System.in);
+
+        //output the prompt
+        System.out.println("Enter an integer: ");
+        //wait for the user to enter value
+
+        String value = input.nextLine();
+
+        //print user input
+        System.out.println("You entered: " + value);
 
     }
 }
