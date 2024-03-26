65dbe18f8b7e0f827b03c875153d3697881cbe69
serena.peschiaroli <serena.peschiaroli@gmail.com>
2024-03-26 14:21:19 +0100

    Add: assignment print statement plus some tests on String and for loop

---
+++ src/org/learning/HelloJava.java
@@ -0,0 +1,17 @@
+package org.learning;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        //test with String
+        String greetings = "Hello, Java!";
+        int myInteger = 10;
+        System.out.println(greetings + " " + "My number is " + myInteger);
+
+        //test with for loop
+
+        for(int i=0; i < 5; i++){
+            System.out.printf("The value of i is: %d\n", i);
+        }
+
+    }
+}
